9fdc7fda8707acae70bf0c4a66af7aeee0508186
Adewoye Damilare <47108141+dareadew@users.noreply.github.com>
2019-02-18 20:24:01 +0400

    Update Order.java
    Order Class

--- src/main/java/CoffeeShop/Order.java
+++ src/main/java/CoffeeShop/Order.java
@@ -1,3 +1,53 @@
 
+
 package CoffeeShop;
+
+/**
+ *
+ * @author Dare Adewoye
+ */
+
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.Date;
+
+class Order{
+    
+    int customerId;
+    Name name;
+    Customer customer;
+    String menuId;
+    double cost;
+    Menu menu;
+    MenuItem menuItem;
+    
+    public Order(String itemId, double cost){
+        
+        menu = new Menu();
+        menu.readMenuFile("data1.csv");  
+        if(menu.searchID(menuId)!= null){
+         this.menuId = itemId;
+         this.cost = menu.searchID(itemId).getCost();
+        }
+            
+    }
+    
+    public String getMenuId(){
+    return menuId;
+    }
+    
+    public double getCost(){
+    return cost;
+    }
+    
+    public String toString(){
+    Date d = new Date();    
+    return menuId + cost + d.toString();
+    }
+    
+    }
+
 

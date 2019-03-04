5ace44f6f74da7a52902e664893d39a1002f63c3
timkofu <makobu.mwambiriro@gmail.com>
2019-03-04 21:14:09 +0400

    CSM mehods defined

--- src/main/java/CoffeeShop/CoffeeShopModel.java
+++ src/main/java/CoffeeShop/CoffeeShopModel.java
@@ -12,17 +12,30 @@ import java.util.ArrayList;
  */
 public class CoffeeShopModel{
 
+    // Get the
+
     /**
      * Open given file and return a file object
      */
     private File getFileHandle(String fileName){
         try{
+            
             ClassLoader classLoader = new Menu().getClass().getClassLoader();
             File file = new File(classLoader.getResource(fileName).toURI());
+
+            // For none buildtool users on windows
+            String isWindows = System.getProperty("os.name").toLowerCase();
+            if(isWindows.contains("win")){
+                // We are on windows; create windows specific file handle
+            }
+
             return file;
+
         } catch(URISyntaxException e) {
+            
             e.printStackTrace();
             return null;
+
         }
 
     }
@@ -49,16 +62,19 @@ public class CoffeeShopModel{
         return new ArrayList();
       }
 
+      // New orders temporary file
+      private File newOrders = File.createTempFile("newOrders", "txt");
+
       /**
        * Create a temporary file to store fresh orders.
        * This needs to be thread-safe.
        */
-      public synchronized void writeCurrentOrder(ArrayList<?> order){
+      public synchronized void addOrders(ArrayList<?> order){
 
       }
 
       /**
-       * Generate all current orders for the report.
+       * Generate report of past and current orders.
        * @return ArrayList
        */
       public ArrayList<?> generateReport(){

c3e7974dc6dd79e0395bb689c68a2a353a4a14e0
timkofu <makobu.mwambiriro@gmail.com>
2019-03-11 17:02:22 +0400

    CoffeeShopModel-Alpha

--- src/main/java/CoffeeShop/CoffeeShopModel.java
+++ src/main/java/CoffeeShop/CoffeeShopModel.java
@@ -1,15 +1,16 @@
 
 package CoffeeShop;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Path;
+import java.lang.ClassLoader;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.net.URI;
 
-import java.lang.ClassLoader;
 
 /**
  * Offers data services to the CoffeeShop package.
@@ -18,7 +19,26 @@ import java.lang.ClassLoader;
  */
 public final class CoffeeShopModel{
 
-    // Get the
+    // Temporary files, created and deleted on program exit
+    File currentOrders, log;
+
+    public CoffeeShopModel(){
+
+        try{
+
+            // Initialize the temporary files
+            this.currentOrders = File.createTempFile("currentOrders", "csv");
+            this.log = File.createTempFile("log", "csv");
+
+            // Set them to delete on program exit
+            this.currentOrders.deleteOnExit();
+            this.log.deleteOnExit();
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
 
     /**
      * Open given file and return an array iterator.
@@ -26,26 +46,19 @@ public final class CoffeeShopModel{
     private ArrayList<String[]> getCSVAsArrays(String fileName){
 
         ClassLoader classLoader = this.getClass().getClassLoader();
-        Object filePath = null;
-        try{
-            filePath = (URI)classLoader.getResource(fileName).toURI();
-        } catch(URISyntaxException e){
-            e.printStackTrace();
-        }
-
         ArrayList<String[]> wholeCSV = new ArrayList<>();
 
 
-        // For none buildtool on windows
-        String isWindows = System.getProperty("os.name").toLowerCase();
-        if(isWindows.contains("win")){
-            // We are on windows; create windows specific file handle
-            // redefine filePath as a string
-            //String filePath = System.getProperty("user.dir")+File.separator+"data"+File.separator+fileName;
-            // Can someone on windows confirm this is working?
-        }
+        // // For none buildtool on windows
+        // String isWindows = System.getProperty("os.name").toLowerCase();
+        // if(isWindows.contains("win")){
+        //     // We are on windows; create windows specific file handle
+        //     // redefine filePath as a string
+        //     //String filePath = System.getProperty("user.dir")+File.separator+"data"+File.separator+fileName;
+        //     // Can someone on windows confirm this is working?
+        // }
 
-        try(Scanner inputStream = new Scanner(new File(filePath))){
+        try(Scanner inputStream = new Scanner(new File(classLoader.getResource(fileName).toURI()))){
 
             while(inputStream.hasNext())
                 wholeCSV.add(inputStream.next().split(","));
@@ -64,8 +77,24 @@ public final class CoffeeShopModel{
     /**
      * Takes a two dimensional array and appends it to the given file.
      * This needs to be thread-safe.
+     * This locks the entire instance when run
      */
-    private synchronized void writeArrayToCSV(String fileName){
+    private synchronized void writeArrayToCSV(String[] toWrite, String temporaryFileName) throws IOException {
+        
+        FileWriter fileToWriteTo = null;
+
+        switch(temporaryFileName){
+            case "orders":
+                fileToWriteTo = new FileWriter(this.currentOrders, true); // Append mode
+                break;
+            case "log":
+                fileToWriteTo = new FileWriter(this.log, true); // Append mode
+                break;
+        }
+
+        BufferedWriter writer = new BufferedWriter(fileToWriteTo);
+        writer.write(String.join(",", toWrite)+System.lineSeparator());
+        writer.close();
 
     }
 
@@ -85,51 +114,79 @@ public final class CoffeeShopModel{
     /**
      * Returns ArrayList of menu items to the caller.
      */
-     public ArrayList<?> getMenuItems(){
-        return new ArrayList();
-     }
+    public ArrayList<String[]> getMenu(){
+        return getCSVAsArrays("menu.csv");
+    }
 
      /**
       * Returns ArrayLits of previous orders.
       */
-      public String[] getPreviousOrders(){
-        return new String[]{"Hi", "There"};
-      }
+    public ArrayList<String[]> getPreviousOrders(){
+        return getCSVAsArrays("orederList.csv");
+    }
 
-      // New orders temporary file
-      //private File newOrders = File.createTempFile("newOrders", "txt");
+    // New orders temporary file
+    //private File newOrders = File.createTempFile("newOrders", "txt");
 
-      /**
-       * Create a temporary file to store fresh orders.
-       * This needs to be thread-safe.
-       * Locks entire instance when accessed
-       */
-      public synchronized void addOrders(ArrayList<?> order){
-        //   this.newOrders
-      }
+    /**
+     * Create a temporary file to store fresh orders.
+     * Takes and Array of Strings of an array of strings.
+     * Converts that to a csv string and appends to the current orders
+     * This needs to be thread-safe.
+     * Locks entire instance when accessed
+     */
+    public void addOrders(String[] order){
+        try{
+            writeArrayToCSV(order, "order");
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 
-      /**
-       * Generate report of past and current orders.
-       * @return ArrayList
-       */
-      public ArrayList<?> getAllOrders(){
-          return new ArrayList();
-      }
+    /**
+     * Return all current orders.
+     * @return ArrayList
+     */
+    public ArrayList<String[]> getOrders(){
+        ArrayList<String[]> currentOrders = new ArrayList<>();
+        try(Scanner reader = new Scanner(this.currentOrders)){
+            while(reader.hasNext()){
+                currentOrders.add(reader.next().split(","));
+            }
+            return currentOrders;
+        } catch(IOException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-      /**
-       * Write to log (requirement 5)
-       * Locks entire instance when accessed
-       */
-      public synchronized void writeToLog(ArrayList<String> toLog){
+    /**
+     * Write to log (requirement 5)
+     * Locks entire instance when accessed
+     */
+    public void writeToLog(String[] toLog){
+        try{
+            writeArrayToCSV(toLog, "log");
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 
-      }
-
-      /**
-       * Read from the log
-       */
-      public ArrayList<String> readLog(){
-          return new ArrayList();
-      }
+    /**
+     * Read from the log
+     */
+    public ArrayList<String[]> readLog(){
+        ArrayList<String[]> logs = new ArrayList<>();
+        try(Scanner reader = new Scanner(this.log)){
+            while(reader.hasNext()){
+                logs.add(reader.next().split(","));
+            }
+            return logs;
+        } catch(IOException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 
 }
